1530136e4ef855ce07eb9026b9c2c1cf6019ff2d
1000zoo <cjswldn99@gmail.com>
2023-11-17 19:24:24 +0900

    Section 2.4: 엔티티 클래스 개발 2

--- src/main/java/zoo/jpashop/domain/item/Item.java
+++ src/main/java/zoo/jpashop/domain/item/Item.java
@@ -7,8 +7,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
+import jakarta.persistence.ManyToMany;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import zoo.jpashop.domain.Category;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -25,5 +29,8 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 
 }
