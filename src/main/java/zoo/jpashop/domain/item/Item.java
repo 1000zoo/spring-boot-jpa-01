eac67f23cd76d3f59079bc93b9e9b7bb895903aa
1000zoo <cjswldn99@gmail.com>
2023-11-21 04:56:35 +0900

    Section 5: 상품 도메인 개발

--- src/main/java/zoo/jpashop/domain/item/Item.java
+++ src/main/java/zoo/jpashop/domain/item/Item.java
@@ -13,6 +13,7 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 import zoo.jpashop.domain.Category;
+import zoo.jpashop.exception.NotEnoughStockException;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
@@ -33,4 +34,15 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    public void addStock(int stockQuantity) {
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int stockQuantity) {
+        int restStock = this.stockQuantity - stockQuantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity += stockQuantity;
+    }
 }
