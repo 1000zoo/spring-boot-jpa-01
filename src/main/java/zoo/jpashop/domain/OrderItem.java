59866b059d04c3e57c60974e0c7ab606f4ccfb74
1000zoo <cjswldn99@gmail.com>
2023-11-22 20:39:34 +0900

    Section 6: 주문, 주문상품 엔티티 개발

--- src/main/java/zoo/jpashop/domain/OrderItem.java
+++ src/main/java/zoo/jpashop/domain/OrderItem.java
@@ -32,4 +32,22 @@ public class OrderItem {
 
     private int orderPrice; //주문 당시의 가격
     private int count;  //주문 당시의 수량
+
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    public void cancel() {
+        getItem().addStock(count);
+    }
+
+    public int getTotalPrice() {
+        return orderPrice * count;
+    }
 }
