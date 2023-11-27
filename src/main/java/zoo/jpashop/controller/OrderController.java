e60e4b730c6fa1d5e7602e809674adef964bbe55
1000zoo <cjswldn99@gmail.com>
2023-11-28 04:30:19 +0900

    Section 7.8: 상품 주문

---
+++ src/main/java/zoo/jpashop/controller/OrderController.java
@@ -0,0 +1,44 @@
+package zoo.jpashop.controller;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import zoo.jpashop.domain.Member;
+import zoo.jpashop.domain.item.Item;
+import zoo.jpashop.service.ItemService;
+import zoo.jpashop.service.MemberService;
+import zoo.jpashop.service.OrderService;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping("/order")
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+
+        orderService.order(memberId, itemId, count);
+        return "redirect:/orders";
+    }
+}
